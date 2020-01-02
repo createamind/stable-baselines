663f2cd71560c53ebe01e41e560386dd9568f50f
gu <zlw21gxy@gmail.com>
2020-01-02 21:54:04 +0800

    exp

--- stable_baselines/sqn/run_script.py
+++ stable_baselines/sqn/run_script.py
@@ -6,6 +6,16 @@ from stable_baselines import SQN
 from stable_baselines.logger import configure
 from stable_baselines.common.cmd_util import make_atari_env
 from stable_baselines.common.vec_env import VecFrameStack
+import os
+target = 0.3
+start = 10000
+ent_coef = "auto"
+name = f"EXP5_1e-3_sqn_Pong_{ent_coef}_{target}_start_{start}_s0"
+os.environ['OPENAI_LOGDIR'] = '../../data/debug/' + name
+os.environ['OPENAI_LOG_FORMAT'] = 'stdout,log,csv,tensorboard'
+os.environ["LD_LIBRARY_PATH"] = "/usr/local/cuda-9.0/lib64:/usr/local/cuda-9.0/extras/CUPTI/lib64"
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
+configure()
 # configure()
 
 
@@ -22,13 +32,14 @@ class CustomSQNPolicy(FeedForwardPolicy):
 
 
 # env = gym.make('Pendulum-v0')
-env = gym.make('CartPole-v0')
+# env = gym.make('CartPole-v0')
 # env = gym.make('LunarLander-v2')
 # env = DummyVecEnv([lambda: env])
-# env = make_atari_env('PongNoFrameskip-v4', num_env=1, seed=0)
+env = make_atari_env('PongNoFrameskip-v4', num_env=1, seed=0)
 # Frame-stacking with 4 frames
-# env = VecFrameStack(env, n_stack=4)
-model = SQN(CustomSQNPolicy, env, learning_rate=0.001, verbose=1, seed=2, ent_coef=0.2)
+env = VecFrameStack(env, n_stack=4)
+model = SQN(CnnPolicy, env, learning_rate=1e-3, verbose=1, seed=0, ent_coef="auto", target_entropy=target,
+            buffer_size=int(1e6), learning_starts=start)
 # Train the agent
 model.learn(total_timesteps=int(2e6))
 
